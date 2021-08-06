0033ef75287a1c2bc55a1cb0780ee9bf09b5b84f
Fabien LOISON <flo@flogisoft.com>
2021-08-06 11:04:00 +0200

    Handle JPEG decompression errors

--- mozjpeg_lossless_optimization/mozjpeg_opti.c
+++ mozjpeg_lossless_optimization/mozjpeg_opti.c
@@ -1,5 +1,23 @@
+#include <setjmp.h>
 #include "../mozjpeg/cdjpeg.h"
 #include "./mozjpeg_opti.h"
+
+
+struct custom_jpeg_error_mgr {
+    struct jpeg_error_mgr pub;
+    jmp_buf setjmp_buffer;
+};
+
+
+void mozjpeg_lossless_optimization_error_exit(j_common_ptr cinfo) {
+    struct custom_jpeg_error_mgr* err = (struct custom_jpeg_error_mgr*) cinfo->err;
+    longjmp(err->setjmp_buffer, 1);
+}
+
+
+void mozjpeg_lossless_optimization_emit_message(j_common_ptr cinfo, int msg_level) {
+    // Suppress message writing on stderr...
+}
 
 
 unsigned long mozjpeg_lossless_optimization(
@@ -7,17 +25,30 @@ unsigned long mozjpeg_lossless_optimization(
     unsigned long input_jpeg_length,
     unsigned char **output_jpeg_bytes
 ) {
-    // Initialize the JPEG decompression object with default error handling.
+    // Initialize the JPEG decompression object with custom error handling
     struct jpeg_decompress_struct srcinfo;
-    struct jpeg_error_mgr jsrcerr;
-    srcinfo.err = jpeg_std_error(&jsrcerr);
+
+    struct custom_jpeg_error_mgr cjsrcerr;
+    srcinfo.err = jpeg_std_error(&cjsrcerr.pub);
+    cjsrcerr.pub.error_exit = &mozjpeg_lossless_optimization_error_exit;
+    cjsrcerr.pub.emit_message = &mozjpeg_lossless_optimization_emit_message;
+
     jpeg_create_decompress(&srcinfo);
 
-    // Initialize the JPEG compression object with default error handling.
+    // Initialize the JPEG compression object with default error handling
     struct jpeg_compress_struct dstinfo;
+
     struct jpeg_error_mgr jdsterr;
     dstinfo.err = jpeg_std_error(&jdsterr);
+
     jpeg_create_compress(&dstinfo);
+
+    // Handle decompression errors
+    if (setjmp(cjsrcerr.setjmp_buffer)) {
+        jpeg_destroy_compress(&dstinfo);
+        jpeg_destroy_decompress(&srcinfo);
+        return 0;
+    }
 
     // Set compression options
     dstinfo.optimize_coding = TRUE;
--- tests/test_mozjpeg_opti.py
+++ tests/test_mozjpeg_opti.py
@@ -21,3 +21,11 @@ class Test_optimize(object):
         result = mozjpeg_lossless_optimization.optimize(image_bytes)
         assert len(result) > 0
         assert len(result) < len(image_bytes)
+
+    def test_optimize_raise_error_with_troncated_input_jpeg(self):
+        with pytest.raises(ValueError):
+            mozjpeg_lossless_optimization.optimize(b"\xFF\xD8\xFF\xE0\x00\x10JFIF")
+
+    def test_optimize_raise_error_with_invalid_input_data(self):
+        with pytest.raises(ValueError):
+            mozjpeg_lossless_optimization.optimize(b"foobar")
